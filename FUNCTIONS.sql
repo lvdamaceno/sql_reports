8098c1f7f5f71f21ad3e2c4693fbc3173148f5a3
Luís Vinícius Damaceno <lvdamaceno@gmail.com>
2018-08-02 15:52:32 -0300

    Update FUNCTIONS.sql

--- FUNCTIONS.sql
+++ FUNCTIONS.sql
@@ -1,6 +1,6 @@
--- funação para formatação do CPF
+-- função para formatação do CPF
 -- chamada da função
--- sankhya].[formatarCPF](CPF)
+-- ([sankhya].[formatarCPF](CPF)) [CPF]
 
 USE SANKHYA_PROD	
 GO				
@@ -11,4 +11,34 @@ BEGIN
 DECLARE @RETORNO VARCHAR(14)			 
 SET @RETORNO = SUBSTRING(@CPF,1,3) + '.' + SUBSTRING(@CPF,4,3) + '.' + SUBSTRING(@CPF,7,3) + '-' + SUBSTRING(@CPF,10,2) 			 
 RETURN @RETORNO		
+END
+
+-- função para formatação do CNPJ
+-- chamada da função
+-- ([sankhya].[formatarCNPJ](CNPJ))[CNPJ]
+
+USE SANKHYA_PROD	
+GO				
+CREATE FUNCTION FORMATARCNPJ(@CNPJ CHAR(14))
+RETURNS CHAR(18)
+AS
+BEGIN 
+DECLARE @RETORNO VARCHAR(18)
+SET @RETORNO = SUBSTRING(@CNPJ ,1,2) + '.' + SUBSTRING(@CNPJ ,3,3) + '.' + SUBSTRING(@CNPJ ,6,3) + '/' + SUBSTRING(@CNPJ ,9,4) + '-' + SUBSTRING(@CNPJ ,13,2)
+RETURN @RETORNO
+END
+
+-- função para formatação do CEP
+-- chamada da função
+-- ([sankhya].[formatarCEP](CEP)) [CEP]
+
+USE SANKHYA_PROD	
+GO				
+CREATE FUNCTION FORMATARCEP(@CEP CHAR(8))
+RETURNS CHAR(10)
+AS
+BEGIN 
+DECLARE @RETORNO VARCHAR(10)
+SET @RETORNO = SUBSTRING(@CEP, 1, 2) + '.' + SUBSTRING(@CEP, 3, 3) + '-' + SUBSTRING(@CEP, 6, LEN(@CEP))
+RETURN @RETORNO
 END
